0f78e8ab6fef39b38f8bc4658812585a44360a56
shoury-dev <115803558+shoury-dev@users.noreply.github.com>
2024-03-31 10:59:10 +0530

    updated data

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080; 
 
 server.use(middlewares);
 server.use(router);
